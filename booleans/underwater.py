902f557dbb34381cfacaf2995919259cf66e397e
[David] <[Deubank671@lsr7.net]>
2017-11-28 10:39:56 -0600

    pushing new files

---
+++ booleans/underwater.py
@@ -0,0 +1,11 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+pos = mc.player.getPos()
+x = pos.x
+y = pos.y
+z = pos.z
+
+blockType == 9
+mc.getBlockType(x, y, z)
+mc.postToChat(blockType == 0)
