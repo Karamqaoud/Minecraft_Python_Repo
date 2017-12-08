76d1b9acb56896c75d5b90e61d6d603dec38ed43
[Karam] <[Kqaoud599@lsr7.net]>
2017-12-08 16:45:28 +0000

    new push

--- ifStatements/secretDoor.py
+++ ifStatements/secretDoor.py
@@ -1,18 +1,24 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-x = 2.7
-y = 15.0
-z = -42.4
+x = -8.3
+y = 11.0
+z = -28.9
 
 gift = mc.getBlock(x, y, z)
 if gift != 0:
     if gift == 57:
-        x = 3.7
-        y = 14.0
-        z = -41.7
+        x = -7.4
+        y = 10.0
+        z = -27.5
         blockType = 0
-        mc.setBlocks(x, y, z, blockType)
+        mc.setBlock(x, y, z, blockType)
+        x = -7.4
+        y = 10.0
+        z = -27.5
+        blockType = 0
+        mc.setBlock(x, y + 1, z, blockType)
+
 
 else:
     mc.postToChat("Place an offering on the pedestal.")
