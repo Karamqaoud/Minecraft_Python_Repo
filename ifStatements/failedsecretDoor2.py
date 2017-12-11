7cbc253ee90f6741ca064fb1ce604462fa9ba452
[Karam] <[Kqaoud599@lsr7.net]>
2017-12-11 17:03:47 +0000

    pushing new files

--- ifStatements/failedsecretDoor2.py
+++ ifStatements/failedsecretDoor2.py
@@ -1,27 +1,23 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-x = 24.5
-y = 1.0
-z = 39.7
+x = -8.3
+y = 11.0
+z = -28.9
 
 gift = mc.getBlock(x, y, z)
 if gift != 0:
     if gift == 57:
-        x = 23.5
-        y = 0.0
-        z = 40.7
+        x = -7.4
+        y = 10.0
+        z = -27.5
         blockType = 0
         mc.setBlock(x, y, z, blockType)
-        x = 23.5
-        y = 0.0
-        z = 40.7
+        x = -7.4
+        y = 10.0
+        z = -27.5
         blockType = 0
         mc.setBlock(x, y + 1, z, blockType)
-    if gift != 57:
-        mc.player.getPos()
-        mc.setBlock(x, y - 1, z, 10)
-        
 
 
 else:
